1ecf3e1b2439cb77251f8e138e34ffba40aa9dc6
Scott Meech <49878063+smeechncar@users.noreply.github.com>
2026-05-01 08:12:19 -0600

    Refactor config.hpp by removing unused structs and includes
    removed SpeciesMapping, SpeciesMap, DatasetDescriptor, InventoryConvention, and the species_map_, convention_,       
      descriptor_ fields from SourceConfig

--- examples/include/miem/config.hpp
+++ examples/include/miem/config.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <map>
-#include <optional>
 #include <string>
 #include <vector>
 
@@ -9,45 +7,21 @@ namespace miem {
 
 enum class SourceMode { Offline, Online };
 enum class SourceType { Anthropogenic, Fire, Biogenic, Dust, SeaSalt, Lightning };
-enum class InventoryConvention { ECCAD, Descriptor };
 enum class TemporalInterpolation { Linear, Nearest };
 enum class VerticalInjection { Surface, Plume };
 
-struct SpeciesMapping {
-  std::string inventory_species_;
-  std::string mechanism_species_;
-  double      scaling_factor_ = 1.0;
-};
-
-struct SpeciesMap {
-  std::string                  mechanism_;
-  std::vector<SpeciesMapping>  mappings_;
-};
-
-struct DatasetDescriptor {
-  std::string                        variable_prefix_;
-  std::string                        flux_units_;
-  double                             unit_conversion_factor_ = 1.0;
-  std::string                        time_dimension_;
-  std::string                        cell_dimension_;
-  std::map<std::string, std::string> species_rename_;
-};
-
 struct SourceConfig {
-  std::string                       name_;
-  SourceMode                        mode_                   = SourceMode::Offline;
-  SourceType                        type_                   = SourceType::Anthropogenic;
-  std::string                       file_pattern_;
-  InventoryConvention               convention_             = InventoryConvention::ECCAD;
-  std::optional<DatasetDescriptor>  descriptor_;
-  std::string                       provider_;
-  SpeciesMap                        species_map_;
-  TemporalInterpolation             temporal_interpolation_ = TemporalInterpolation::Linear;
-  VerticalInjection                 vertical_injection_     = VerticalInjection::Surface;
-  int                               category_               = 0;
-  int                               hierarchy_              = 1;
-  double                            scaling_factor_         = 1.0;
-  std::string                       sector_;
+  std::string           name_;
+  SourceMode            mode_                   = SourceMode::Offline;
+  SourceType            type_                   = SourceType::Anthropogenic;
+  std::string           file_pattern_;
+  std::string           provider_;
+  TemporalInterpolation temporal_interpolation_ = TemporalInterpolation::Linear;
+  VerticalInjection     vertical_injection_     = VerticalInjection::Surface;
+  int                   category_               = 0;
+  int                   hierarchy_              = 1;
+  double                scaling_factor_         = 1.0;
+  std::string           sector_;
 };
 
 struct MIEMConfig {
